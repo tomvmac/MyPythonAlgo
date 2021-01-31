e416edff4668ef10d75ba5b1ab2a73e1d4ae6937
Tom <tomvmac@yahoo.com>
2021-01-31 13:33:12 -0500

    Updated file to include delete methods.

--- Ch5-Search/BinarySearch/bst_demo.py
+++ Ch5-Search/BinarySearch/bst_demo.py
@@ -88,67 +88,153 @@ class BSTDemo:
                 return self._find_val(curr.right_child, key)
         return "Value not found in tree"
 
+    def delete_val(self, key):
+        self._delete_val(self.root, None, None, key)
+
+    def _delete_val(self, curr, prev, is_left, key):
+        # Deleting nodes requires attention to the following steps:
+        # 1. Find node with key
+
+        # Conditions
+        # 1. Node has no children or leaf node:
+        #    a.  Delete node or root if there no children or leaf nodes by removing link from parent to leaf node
+        # 2. Node has one child or leaf node:
+        #    a. Make parent of node point to node's child
+        #    b. Point node to None
+        # 3. Node has two children or leaf nodes (choose one):
+        #    a. If Choose left child
+        #        i. Find the max value from the left child
+        #        j. Keep traversing left until you find a node with no right child = max value
+        #        k. Copy max value to node value
+        #        l. Remove max node by pointing it's parent to max node's child and point max node left child to None
+        #    b. If Choose right child
+        #        i. Find the min value from the right child
+        #        j. Keep traversing left until you find a node with no left child = min value
+        #        k. Copy min value to node value
+        #        l. Remove min node by pointing it's parent to min node's child and point min node right child to None
+
+        if curr:
+            if key == curr.data:
+                print("Found node to delete")
+                if curr.left_child and curr.right_child:
+                    # both left and right child exits
+                    print("problem scenario")
+                elif curr.left_child == None and curr.right_child == None:
+                    # no child
+                    if is_left:
+                        prev.left_child = None
+                    else:
+                        prev.right_child = None
+                elif curr.left_child == None:
+                    # no left child
+                    if prev:
+                        # Make parent point to curr node right child
+                        if is_left:
+                            prev.left_child = curr.right_child
+                        else:
+                            prev.right_child = curr.right_child
+                    else:
+                        self.root = curr.right_child
+                elif curr.right_child == None:
+                    # no right child
+                    if prev:
+                        # Make parent point to curr node right child
+                        if is_left:
+                            prev.left_child = curr.left_child
+                        else:
+                            prev.right_child = curr.left_child
+                    else:
+                        self.root = curr.left_child
+            elif key < curr.data:
+                self._delete_val(curr.left_child, curr, True, key)
+            elif key > curr.data:
+                self._delete_val(curr.right_child, curr, False, key)
+
+        else:
+            print(f"{key} not found in Tree.")
+
+
 # Execution Code
 
 tree = BSTDemo()
 
-tree.insert("F")
+# tree.insert("F")
 # print(tree.root.data)
 
-tree.insert("C")
+# tree.insert("C")
 # print(tree.root.left_child.data)
 
-tree.insert("G")
+# tree.insert("G")
 # print(tree.root.right_child.data)
 
-tree.insert("A")
+# tree.insert("A")
 # print(tree.root.left_child.left_child.data)
 
-tree.insert("B")
+# tree.insert("B")
 # print(tree.root.left_child.left_child.right_child.data)
 
-tree.insert("K")
+# tree.insert("K")
 # print(tree.root.right_child.right_child.data)
 
 
-tree.insert("H")
+# tree.insert("H")
 # print(tree.root.right_child.right_child.left_child.data)
 
 
-tree.insert("E")
+# tree.insert("E")
 # print(tree.root.data)
 #
-tree.insert("D")
+# tree.insert("D")
 # print(tree.root.data)
 #
-tree.insert("I")
+# tree.insert("I")
 # print(tree.root.data)
 #
-tree.insert("M")
+# tree.insert("M")
 # print(tree.root.data)
 #
-tree.insert("J")
+# tree.insert("J")
 # print(tree.root.data)
 #
-tree.insert("L")
+# tree.insert("L")
 # print(tree.root.data)
 
-print("in order (left, root, right)\n")
+# print("in order (left, root, right)\n")
+# tree.in_order()
+# print("\n------------------------------\n")
+#
+# print("pre order (root, left, right)\n")
+# tree.pre_order()
+# print("\n------------------------------\n")
+#
+# print("post order (left, right, root)\n")
+# tree.post_order()
+
+# print("search for E")
+# print(tree.find_val("E"))
+#
+# print("search for J")
+# print(tree.find_val("J"))
+#
+# print("search for Z")
+# print(tree.find_val("Z"))
+
+# print("\n------------------------------\n")
+# print("delete Z\n")
+#
+# tree.in_order()
+# tree.del_val("Z")
+# tree.in_order()
+
+print("\n------------------------------\n")
+print("delete F\n")
+tree.insert("F")
+# left child
+tree.insert("C")
+# right child
+tree.insert("G")
+
 tree.in_order()
-print("\n------------------------------\n")
-
-print("pre order (root, left, right)\n")
-tree.pre_order()
-print("\n------------------------------\n")
-
-print("post order (left, right, root)\n")
-tree.post_order()
-
-print("search for E")
-print(tree.find_val("E"))
-
-print("search for J")
-print(tree.find_val("J"))
-
-print("search for Z")
-print(tree.find_val("Z"))
+# tree.delete_val("C")
+tree.delete_val("F")
+tree.in_order()
